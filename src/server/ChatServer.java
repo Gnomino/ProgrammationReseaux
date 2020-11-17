4ae5ce532c2b7a921ee1e26a0ecf9de1a2e69dbe
sylvain vaure <sylvain.vaure@insa-lyon.fr>
2020-11-17 12:10:44 +0200

    Implements basic Server

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,10 +1,42 @@
 package server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatServer {
 
+    private static List<ClientThread> clientThreads = new ArrayList<>();
 
+    public static void main(String args[]){
+        ServerSocket listenSocket;
 
-    public static void main(String[] args) {
-	    // write your code here
+        try {
+            listenSocket = new ServerSocket(1234); //port
+            System.out.println("Server ready...");
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                System.out.println("Connexion from:" + clientSocket.getInetAddress());
+                ClientThread ct = new ClientThread(clientSocket);
+                clientThreads.add(ct);
+                ct.start();
+            }
+        } catch (Exception e) {
+            System.err.println("Error in EchoServer:" + e);
+        }
+    }
+
+    /**
+     * Processes a received message and sends it to other clients
+     * @param message
+     * @param sender
+     */
+    public static void sendMessage(String message, ClientThread sender) {
+        for(ClientThread client : clientThreads) {
+            if(client != sender) {
+                client.sendMessage(message);
+            }
+        }
     }
 }
--- src/server/ClientThread.java
+++ src/server/ClientThread.java
@@ -1,4 +1,46 @@
 package server;
 
-public class ClientThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+    private Socket clientSocket;
+
+    ClientThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    /**
+     * Receives a message from client then passes it to the main thread
+     **/
+    public void run() {
+        try {
+            BufferedReader socIn = null;
+            socIn = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+            while (true) {
+                String line = socIn.readLine();
+                ChatServer.sendMessage(line, this);
+            }
+        } catch (Exception e) {
+            System.err.println("Error in EchoServer:" + e);
+        }
+    }
+
+    /**
+     * Sends a message to the socket of this client
+     * @param message
+     */
+    public void sendMessage(String message) {
+        try {
+            PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
+            socOut.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
